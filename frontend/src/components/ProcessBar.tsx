a89b76e714a970c84b9dc918a8f98d05446e4962
yeonddori <lsymiso@naver.com>
2024-05-19 20:09:27 +0900

    feat: 진행도 상태바 구현

---
+++ frontend/src/components/ProcessBar.tsx
@@ -0,0 +1,30 @@
+import { Box, LinearProgress, Typography } from '@mui/material';
+
+interface ProgressBarProps {
+  current: number;
+  maximum: number;
+}
+
+const ProgressBar = ({ current, maximum }: ProgressBarProps) => {
+  return (
+    <Box sx={{ margin: '50px', width: 300 }}>
+      <LinearProgress
+        variant="determinate"
+        value={(current / maximum) * 100}
+        sx={{
+          borderRadius: '50px',
+          height: '20px',
+          bgcolor: 'gray',
+          '& .MuiLinearProgress-bar': {
+            bgcolor: 'black',
+          },
+        }}
+      />
+      <Typography variant="body1" sx={{ fontWeight: 'bold', textAlign: 'center', marginTop: '5px' }}>
+        {`${current} / ${maximum}`}
+      </Typography>
+    </Box>
+  );
+};
+
+export default ProgressBar;
