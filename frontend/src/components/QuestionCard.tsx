bd751e93dcf358d8d3e1923b0bbc03fb0560746a
yeonddori <lsymiso@naver.com>
2024-05-19 20:12:05 +0900

    feat: 질문카드 구현

---
+++ frontend/src/components/QuestionCard.tsx
@@ -0,0 +1,28 @@
+import { Box, Typography } from '@mui/material';
+
+interface QuestionCardProps {
+  id: number;
+  content: string;
+}
+
+const QuestionCard = ({ id, content }: QuestionCardProps) => {
+  return (
+    <Box
+      sx={{
+        border: '3px solid #000',
+        borderRadius: '10px',
+        padding: '10px',
+        marginBottom: '10px',
+        width: 500,
+        height: 200,
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Q{id}
+      </Typography>
+      <Typography variant="h5">{content}</Typography>
+    </Box>
+  );
+};
+
+export default QuestionCard;
